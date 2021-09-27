5cd9c07f99293d0219b6ed6d5c2747fe28336891
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-27 02:51:20 +0200

    Rework of campaign-item component.

--- src/app/campaign-item/campaign-item.component.ts
+++ src/app/campaign-item/campaign-item.component.ts
@@ -1,18 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Campaign} from "../campaign";
 
 @Component({
-  selector: 'app-campaign-item',
-  templateUrl: './campaign-item.component.html',
-  styleUrls: ['./campaign-item.component.css']
+  selector: 'campaign-item',
+  styleUrls: ['./campaign-item.component.scss'],
+  template: `
+    <div class="campaign-item">
+      <div class="campaign-header">{{campaign.name}}</div>
+      <div class="content">
+        <div class="content-left">
+          <p>Location: {{campaign.city}}</p>
+          <p>Distance: {{campaign.radius}}</p>
+        </div>
+        <div class="content-right">
+          <p>Current bid: {{campaign.bid.amount}}</p>
+          <p>Place your bid: {{campaign.bid.amount}}</p>
+        </div>
+      </div>
+      <div class="controls">
+        <button class="delete-button" (click)="handleRemove()">x</button>
+      </div>
+    </div>`
 })
-export class CampaignItemComponent implements OnInit {
+export class CampaignItemComponent {
   @Input() campaign!: Campaign;
+  @Output() onRemove = new EventEmitter()
+  @Output() onClick = new EventEmitter()
 
-  constructor() {
+  handleClick() {
+    this.onClick.emit(this.campaign.id)
   }
 
-  ngOnInit(): void {
+  handleRemove() {
+    this.onRemove.emit(this.campaign.id)
   }
-
 }
