e067fbab8387941d48cc2671e0c00bb493fa1920
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-27 02:57:07 +0200

    Declutter app-component and delegate displaying list to another component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,31 @@
-import {Component} from '@angular/core';
-import {Campaign, City} from "./campaign";
+import {Component, ViewChild} from '@angular/core';
+import {Campaign} from "./campaign";
+import API from "./API";
+import {CampaignListComponent} from "./campaign-list/campaign-list.component";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>{{title}}</h1>
+    <div class="content-wrapper">
+      <form-container (formSubmitted)="onCampaignFormSubmitted($event)"></form-container>
+      <campaign-list #list></campaign-list>
+    </div>
+  `,
+
 })
+
 export class AppComponent {
   title = 'shop-app';
-  campaigns: Array<Campaign> = Array()
+  @ViewChild("list") campaignListComponent!: CampaignListComponent
+  shouldShowForm: boolean = true
 
-  constructor() {
-    this.campaigns = []
-    for(let i = 0; i < random(5,10); i++) {
-      const campaign = randomCampaign()
-      console.log(campaign)
-      this.campaigns.push(campaign)
-    }
-    console.log("campaigns count: " + this.campaigns.length)
+  onCampaignFormSubmitted = async (campaign: Campaign) => {
+    const result = await API.insertCampaign(campaign).catch(err => console.log(err))
+    this.campaignListComponent.onCampaignAdded(campaign)
+    console.log(result)
   }
-}
-
-const random = (min: number = 0, max: number = 100) =>
-  Math.floor(min + Math.random() * (max - min))
-
-const randomCampaign = (): Campaign => {
-  const randomCity: City = (() => {
-    const values = Object.keys(City);
-    const enumKey = values[Math.floor(Math.random() * values.length)];
-    // @ts-ignore
-    return City[enumKey];
-  })()
 
 
-  const minBid = random(1000, 5000)
-
-  return {
-    bid: {amount: Math.random() > 0.5 ? (Math.random() + 1) * minBid : minBid, minAmount: minBid},
-    campaignFund: 0,
-    keywords: [],
-    name: `Random name ${random(100000, 999999)}`,
-    radius: random(5, 150),
-    status: Math.random() > 0.5,
-    town: randomCity
-  }
 }
