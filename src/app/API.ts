7e6f2ea2b655df087e6ad53921843dfd54767b40
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-29 03:50:58 +0200

    API: Add methods for user login and register with mocking.

--- src/app/API.ts
+++ src/app/API.ts
@@ -1,5 +1,7 @@
-import axios from "axios"
+import axios, {AxiosResponse} from "axios"
 import MockAdapter from "axios-mock-adapter"
+import {User} from "./user";
+import {Campaign} from "./campaign";
 
 const api = axios.create({
   baseURL: "/api"
@@ -20,11 +22,19 @@ export default class API {
     headers: {"content-type": "multipart/form-data"}
   })
 
+  static login = (data: User) => api.post("/login", data, {
+    headers: {"content-type": "multipart/form-data"}
+  })
+
+  static register = (data: { login: string, password: string }) => api.post("/register", data, {
+    headers: {"content-type": "multipart/form-data"}
+  })
 }
 
 // mocking
 const mock = new MockAdapter(api)
 import campaigns from "../assets/mock-data/campaigns.json"
+import users from "../assets/mock-data/users.json"
 
 mock.onGet("/campaigns").reply(200, campaigns)
 
@@ -47,4 +57,34 @@ mock.onDelete("/campaign").reply(({params: {id}}) => {
     return [400]
   campaigns.splice(campaigns.indexOf(found), 1)
   return [200, found.id]
+})
+
+mock.onPut("/campaign").reply(({data, params}) => {
+  const updatedData = JSON.parse(data)
+  const found = campaigns.find(el => el.id == params.id)
+  if (!found) {
+    return [400]
+  }
+  const idx = campaigns.indexOf(found)
+  Object.keys(updatedData).forEach(key => {
+    if (key === "currentBid")
+      found.currentBid = updatedData.currentBid
+  })
+  campaigns[idx] = found
+  return [200]
+})
+
+mock.onPost("/login").reply(({data: {login, password}}) => {
+  const found = users.find(el => el.login == login && el.password == password)
+  if (!found)
+    return [400, "Wrong user and/or password"]
+  return [200]
+})
+
+mock.onPost("/register").reply(({data}) => {
+  const found = users.find(el => el.login == data.login)
+  if (found)
+    return [400, "User with given name already exists"]
+  users.push(JSON.parse(data))
+  return [200]
 })
