03c39af05742b9e78aae7cd5565ead61efe180d6
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-29 04:01:01 +0200

    Add filtering of displayed campaigns with provided name filter.
    It filters original campaigns array and makes filtered one to be displayed.

--- src/app/campaign-list/campaign-list.component.ts
+++ src/app/campaign-list/campaign-list.component.ts
@@ -7,22 +7,39 @@ import API from "../API";
   styleUrls: ["./campaign-list.component.scss"],
   template: `
     <div class="campaign-wrapper">
-      <div class="campaign-list-header">List of campaigns</div>
       <div class="campaign-list">
-        <campaign-item *ngFor="let campaign of campaigns.reverse()" [campaign]="campaign" (onRemove)="onCampaignRemoved($event)"></campaign-item>
+        <campaign-item *ngFor="let campaign of filteredCampaigns.reverse()" [campaign]="campaign" (onRemove)="onCampaignRemoved($event)"></campaign-item>
       </div>
     </div>`
 })
 export class CampaignListComponent implements OnInit {
+  nameFilter?: string
   campaigns: Array<Campaign> = Array()
+  filteredCampaigns: Array<Campaign> = Array()
 
   async ngOnInit() {
     const result = await API.getCampaigns()
     this.campaigns = result.data.map((el: Campaign) => el)
+    this.filterCampaigns()
   }
 
-  onCampaignAdded = (campaign: Campaign) => {
+  filterCampaigns() {
+    this.filteredCampaigns = this.campaigns.filter((el: Campaign) => {
+      return (
+        el.name.toLowerCase().match(this.nameFilter?.toLowerCase() || "") ||
+        el.keywords.reduce((acc: boolean, curr: string) => curr.toLowerCase().match(this.nameFilter?.toLowerCase() || "") ? true : acc, false)
+      )
+    })
+  }
+
+  addCampaign = (campaign: Campaign) => {
     this.campaigns.push(campaign)
+    this.filterCampaigns()
+  }
+
+  updateFilter = (newFilter: string) => {
+    this.nameFilter = newFilter
+    this.filterCampaigns()
   }
 
   onCampaignRemoved = async (id: string) => {
@@ -34,5 +51,6 @@ export class CampaignListComponent implements OnInit {
       if (found)
         this.campaigns.splice(this.campaigns.indexOf(found), 1)
     }
+    this.filterCampaigns()
   }
 }
